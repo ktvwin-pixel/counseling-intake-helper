8cf16e4fc5dabcadef72293c1c852e3279ecc8b2
ktvwin-pixel <ktvwin@gmail.com>
2026-08-12 21:58:52 +0900

    Configure Apps Script web app URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbw5xm6w4uGq3-WlHFL-HtlWvw7GEBOdnW85Df9cb_axsbta1Oxeic77tyDdMhxKA73zVg/exec",
   recipient: "hanart73@gmail.com",
   accessCode: "HAN-0812",
 };
