270ad0c5793651b381e58520bd7b8d0a24f4adda
ktvwin-pixel <ktvwin@gmail.com>
2026-08-13 10:23:12 +0900

    Update Apps Script web app URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbw5xm6w4uGq3-WlHFL-HtlWvw7GEBOdnW85Df9cb_axsbta1Oxeic77tyDdMhxKA73zVg/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycby8OpZGdF_yJXWjBXchV9Mn4mDuCJ-_ClE-lBaV-1AZsWvrwzn_QGHZTSIjyvCKdQZe3w/exec",
   recipient: "hanart73@gmail.com",
   accessCode: "HAN-0812",
 };
